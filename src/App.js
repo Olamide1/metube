da87a2f04b29df4fc7ecd8eb121c1300f2775f9b
Olamide Akomolafe <olamideakomolafe1234@gmail.com>
2019-03-28 13:28:50 +0000

    updated the imports

--- src/App.js
+++ src/App.js
@@ -1,53 +1,54 @@
 import React from "react";
 import { BrowserRouter } from 'react-router-dom';
-import { MDBContainer, MDBRow, MDBCol,MDBNavbar, MDBCard,MDBCardBody,MDBCardImage,MDBCardTitle,MDBCardText, MDBNavbarBrand, MDBJumbotron } from "mdbreact";
+import { MDBContainer, MDBRow, MDBCol, MDBNavbar, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBNavbarBrand }
+  from "mdbreact";
 
 export default () => (
   <BrowserRouter>
 
-<MDBContainer fluid>
-<br/>
+    <MDBContainer fluid>
+      <br />
       <MDBRow >
         <MDBCol>
-            <MDBNavbar color="blue" className="text-white darken-3">
+          <MDBNavbar color="blue" className="text-white darken-3">
             <MDBCol md="8">
-        <MDBNavbarBrand>Metube</MDBNavbarBrand>
-        </MDBCol>
-        <MDBCol md="4">
-        <input className="form-control" type="text" placeholder="Search" aria-label="Search" />
-        </MDBCol>
-            </MDBNavbar>
+              <MDBNavbarBrand>Metube</MDBNavbarBrand>
+            </MDBCol>
+            <MDBCol md="4">
+              <input className="form-control" type="text" placeholder="Search" aria-label="Search" />
+            </MDBCol>
+          </MDBNavbar>
         </MDBCol>
       </MDBRow>
-<br/>
+      <br />
 
-<MDBRow fluid>
-        <MDBCol md="8">
+      <MDBRow fluid>
+        <MDBCol md="9">
           <MDBCard>
-            <MDBCardImage cascade className="img-fluid" src="https://mdbootstrap.com/img/Photos/Slides/img%20(137).jpg"/>
+            <MDBCardImage cascade className="img-fluid" src="https://mdbootstrap.com/img/Photos/Slides/img%20(137).jpg" />
             <MDBCardBody>
-            <MDBCardTitle>
-              Current Video
+              <MDBCardTitle>
+                Current Video
             </MDBCardTitle>
-            <MDBCardText>This is the video of the video. Bye!</MDBCardText>
+              <MDBCardText>This is the video of the video. Bye!</MDBCardText>
             </MDBCardBody>
           </MDBCard>
         </MDBCol>
 
-        <MDBCol md="4">
+        <MDBCol md="3">
           <MDBCard>
-  
+
             <MDBCardImage cascade className="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/men.jpg" />
             <MDBCardBody>
               <MDBCardTitle>Push Video</MDBCardTitle>
               <MDBCardText>Video Of Jackue Chan's and Bruce Lee's fight, before Bruce Lee's death.</MDBCardText>
-          
+
             </MDBCardBody>
-         
+
           </MDBCard>
         </MDBCol>
       </MDBRow>
-</MDBContainer>
+    </MDBContainer>
 
   </BrowserRouter>
 );
